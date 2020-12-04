18b477cc6ca09c85465b370aec3c171d7807ca70
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-12-04 15:24:32 +0100

    Add files via upload

---
+++ zad 5/zad 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int karta, zbroj = 0;
+
+         while (zbroj < 31)
+            {
+                Console.WriteLine("Upišite broj karata: ");
+
+                karta = Convert.ToInt32(Console.ReadLine());
+
+                if (karta > 0 && karta <= 13)
+                {
+                    zbroj = karta + zbroj;
+                }
+            }
+            if (zbroj == 31)
+            {
+                Console.WriteLine("Pobjeda");
+            }
+            if (zbroj != 31)
+            {
+                Console.WriteLine("Poraz");
+            }
+            Console.ReadKey();
+        }
+    }
+}
